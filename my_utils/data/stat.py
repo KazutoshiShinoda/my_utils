a2fdfde56e4829cc1159d657dbefd4c03e01e593
shinochin <kazutoshi.shinoda0516@gmail.com>
2021-07-14 16:47:21 +0900

    update stat

--- my_utils/data/stat.py
+++ my_utils/data/stat.py
@@ -1,20 +1,38 @@
+import copy
+
+
 class Statistics(object):
     def __init__(self):
-        self.num_update = 0
-        self.stat = {}
+        self.global_update = 0
+        self.previous_update = 0
+        self.global_stat = {}
+        self.previous_stat = {}
 
     def update_dict(self, stat_dict):
         for key, value in stat_dict.items():
-            self.stat[key] = self.stat.get(key, 0) + value
-        self.num_update += 1
+            self.global_stat[key] = global_self.stat.get(key, 0) + value
+        self.global_update += 1
 
     def mean(self):
         mean_stat = {}
-        for key, value in self.stat.items():
-            mean_stat[key] = value / self.num_update
-        mean_stat['NumOfUpdates'] = self.num_update
+        for key, value in self.global_stat.items():
+            mean_stat[key] = value / self.global_update
+        mean_stat['NumOfUpdates'] = self.global_update
+        return mean_stat
+
+    def loccal_mean(self):
+        mean_stat = {}
+        for key, value in self.global_stat.items():
+            mean_stat[key] = (value - self.previous_stat.get(key, 0)) / \
+                (self.global_update - self.previous_update)
+
+        self.previous_update = self.global_update
+        self.previous_stat = copy.deepcopy(self.global_stat)
+
         return mean_stat
 
     def reset(self):
-        self.num_update = 0
-        self.stat = {}
+        self.global_update = 0
+        self.previous_update = 0
+        self.global_stat = {}
+        self.previous_stat = {}
